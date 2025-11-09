4fc76703ec88ca9e67431885095bc1319286254d
ItakawaM <maksymworkp@gmail.com>
2025-11-09 16:55:27 +0200

    Day 3 CSS Variables

---
+++ CSS Variables/variables.js
@@ -0,0 +1,14 @@
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdates() {
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    `${this.value}${suffix}`
+  );
+}
+
+inputs.forEach((input) => input.addEventListener("change", handleUpdates));
+inputs.forEach((input) => input.addEventListener("input", handleUpdates));
+
+inputs.forEach((input) => handleUpdates.call(input));
